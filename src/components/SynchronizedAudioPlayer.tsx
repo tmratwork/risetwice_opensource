a0fda8f82dbe0a92bbbc98d9602907c1cc3efd5e
tmratwork <tmratwork@gmail.com>
2025-11-05 01:22:12 -0500

    UI

--- src/components/SynchronizedAudioPlayer.tsx
+++ src/components/SynchronizedAudioPlayer.tsx
@@ -439,7 +439,7 @@ export const SynchronizedAudioPlayer: React.FC<SynchronizedAudioPlayerProps> = (
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
               </svg>
-              Play Combined Recording
+              Play Recording
             </>
           )}
         </button>
